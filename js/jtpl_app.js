269b5e4331bf74697aa9b524fac3f106057b83dd
tarnawa <tarnawa@optonline.net>
2015-08-28 07:42:27 -0400

    nnb

--- js/jtpl_app.js
+++ js/jtpl_app.js
@@ -264,8 +264,17 @@ $( "#bcode" ).append(detlist_html);
 $(document).ready(function(){
 
 //open in app browser
-$('#onlinebtn').on('click', function () {
+$('#3m_btn').on('click', function () {
 window.open('http://ebook.3m.com/library/jtpl/Featured', '_blank', 'location=yes');
+});
+$('#zinio').on('click', function () {
+window.open('https://www.rbdigital.com/mainincnj/service/zinio/landing?', '_blank', 'location=yes');
+});
+$('#oneclick_btn').on('click', function () {
+window.open('http://jeffersontwpnj.oneclickdigital.com/', '_blank', 'location=yes');
+});
+$('#freegal_btn').on('click', function () {
+window.open('http://jeffersonlibrary.freegalmusic.com/homes/index', '_blank', 'location=yes');
 });
 //google map
 
