2612d9b09574235bd32fde793bad54bf8a878ea1
tarnawa <tarnawa@optonline.net>
2015-09-29 07:10:30 -0400

    gert

--- js/jtpl_app_new.js
+++ js/jtpl_app_new.js
@@ -158,11 +158,10 @@ $( "#bcode" ).append(detlist_html);
 
 $(document).ready(function(){
 
-$('#events_btn').on('click', function () {
+//$('#events_btn').on('click', function () {
 //$('#theframes').load("http://jeffersonlibrary.net/forms/eventsprobe_all_app.php");
-
-$('iframe').attr('src', $('iframe').attr('src'));
-});
+//$('iframe').attr('src', $('iframe').attr('src'));
+//});
 
 //open in app browser
 $('#3m_btn').on('click', function () {
