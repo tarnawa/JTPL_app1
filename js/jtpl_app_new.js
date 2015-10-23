695a3ad15a636cace025c18aac44e78d8eba0a6d
tarnawa <tarnawa@optonline.net>
2015-10-23 14:53:57 -0400

    solution

--- js/jtpl_app_new.js
+++ js/jtpl_app_new.js
@@ -274,9 +274,13 @@ $('#cn_holdreq').val("");
 });
 //Home Button workaround to work with IOS 9
 $('.home').on('click', function () {
+								 
+
+        location.hash = '';
+    
 //alert(window.location.hash.substring(0));								 
 //$.mobile.pageContainer.pagecontainer('change', 'index.html', { transition: "fade", allowSamePageTransition: 'true' });
-self.location.href = 'index.html';
+//self.location.href = 'index.html';
 //location.replace("index.html")
 });
 
