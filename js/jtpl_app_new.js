a13acf0db35a9537d1774521dbce665ade1588d9
tarnawa <tarnawa@optonline.net>
2015-10-31 09:03:29 -0400

    lkjljlljkjlk

--- js/jtpl_app_new.js
+++ js/jtpl_app_new.js
@@ -13,12 +13,11 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 	
-	$.mobile.hashListeningEnabled=false;
 	
 //enable back button in ios9	
-//if(device.platform === "iOS" && parseInt(device.version) === 9){
-       // $.mobile.hashListeningEnabled = false;
-   // }	
+if(device.platform === "iOS" && parseInt(device.version) === 9){
+       $.mobile.hashListeningEnabled = false;
+   }	
 //homepage spacing in iphone6plus	
 //var whatis=navigator.userAgent;
 //alert('this is'+whatis+'');
