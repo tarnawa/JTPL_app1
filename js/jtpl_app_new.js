fe5a9d2a04cee61b56912fce5aa7383c2ef9c181
tarnawa <tarnawa@optonline.net>
2015-11-02 08:15:18 -0500

    Kopenhagen

--- js/jtpl_app_new.js
+++ js/jtpl_app_new.js
@@ -292,15 +292,15 @@ $('#cn_holdreq').val("");
 });
 
 //Home Button workaround to work with IOS 9
-//$('.home').on('click', function () {
+$('.home').on('click', function () {
 //location.hash = '';
 //window.history.back(-1);
 //alert(window.location.hash.substring(0));								 
 //$.mobile.pageContainer.pagecontainer('change', 'index.html', { transition: "fade", allowSamePageTransition: 'true' });
-//self.location.href = 'index.html';
+self.location.href = 'index.html';
 //window.location.href = 'index.html';
 ////location.replace("index.html")
-//});
+});
 
 //setTimeout(function() {
     // Change your html here
